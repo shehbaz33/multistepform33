44a24090a92f8bfc3f82ebbba538c4b6836b4dce
Shehbaz33 <shehbazsayed33@gmail.com>
2022-02-18 20:31:33 +0530

    multistepform

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Shipping from './components/Shipping';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-500 text-3xl container mx-auto flex item-start mt-10'>Enter your details to proceed forward</h1>
+      <h1 className='text-red-500 text-3xl container mx-auto flex item-start mt-10'>Enter your details to proceed...</h1>
       <Shipping/>
     </div>
   );
